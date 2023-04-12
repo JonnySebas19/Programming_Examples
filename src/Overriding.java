218b395af83395b8e9a854b441dd1c547892deb7
Jonathan Sebastiani <jonsebastiani17@gmail.com>
2023-04-12 15:00:28 -0700

    Added to Overriding

--- src/Overriding.java
+++ src/Overriding.java
@@ -2,19 +2,19 @@ public class Overriding {
     private String name;
     @Override
     public boolean equals(Object anotherDog) {
-        //Checking memory address
-        if (this == anotherDog) {
+        if (this == anotherDog){
             return true;
         }
-        //Checks if empty or if the object is from a different class
+        //Checking that another dog is actually an instance of the Overriding class
+        if (!(anotherDog instanceof Overriding)) {
+            return false;
+        }
+        //Checks if empty or if from different classes
         if (anotherDog == null || getClass() != anotherDog.getClass()) {
             return false;
         }
 
-        /*
-        Casting the passed in object as a Dog class
-        We do this because...
-         */
+        //Casting the passed in object as a Dog class
         Overriding dog = (Overriding) anotherDog;
 
         //Return if the contents equal each other
@@ -29,5 +29,10 @@ Notes:
     Java is pass-by-value. This means that when an argument is plugged in for a parameter (of any type),
     the argument is evaluated and the value obtained is used to initialize the value of the parameter.
     A method can change the instance variables of an object given as an argument.
+
+    - Overriding the Object.equals() method that is built into Java. Everything comes from this main class.
+    All classes are secretly...  public class Overriding extends Object{};
+
+    - Doesn't NEED to say @Override, but it will help the compiler to see what you are trying to do.
 
  */
