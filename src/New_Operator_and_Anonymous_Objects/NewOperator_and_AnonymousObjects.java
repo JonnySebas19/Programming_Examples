142561046b99c548c9465beab7382e4bd128c625
Jonathan Sebastiani <jonsebastiani17@gmail.com>
2023-04-12 14:35:06 -0700

    Added null and anonymous objects

--- src/New_Operator_and_Anonymous_Objects/NewOperator_and_AnonymousObjects.java
+++ src/New_Operator_and_Anonymous_Objects/NewOperator_and_AnonymousObjects.java
@@ -3,9 +3,7 @@ package New_Operator_and_Anonymous_Objects;
 public class NewOperator_and_AnonymousObjects {
     public static void main(String[] args){
         ToyClass obj1 = new ToyClass("Joe", 42);
-        ToyClass obj2 = null;
 
-        /*
         // Using the Anonymous Object "new ToyClass(...)"
         if (obj1.equals(new ToyClass("Joe", 42))){
             System.out.println("They are equal");
@@ -13,23 +11,22 @@ public class NewOperator_and_AnonymousObjects {
             System.out.println("They are not equal");
         }
 
-         */
-        System.out.println(obj2);
-        obj2 = new ToyClass("John", 30);
-        System.out.println(obj2);
-
-        /*
-        This is the same as...
-
+        //This is the same as...
         ToyClass otherToy = new ToyClass("Joe", 42);
         if (obj1.equals(otherToy)){
             System.out.println("They are equal");
         } else {
             System.out.println("They are not equal");
         }
-         */
+
+        //Testing printing with null pointer
+        ToyClass obj2 = null;
+        System.out.println(obj2); //Prints: null
+        obj2 = new ToyClass("John", 30);
+        System.out.println(obj2); //Prints: New_Operator_and_Anonymous_Objects.ToyClass@30f39991
     }
 }
+
 /*
 Notes:
     Null : Special constant that can be used as a placeholder for memory address.
